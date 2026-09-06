f3cce350b92e05dd0c6408a564a1678d2826837f
Ikey Doherty <travellingengineer@proton.me>
2026-09-06 22:03:31 +0100

    barney: Basic clap builder integration
    Signed-off-by: Ikey Doherty <travellingengineer@proton.me>

--- barney/src/main.rs
+++ barney/src/main.rs
@@ -1,3 +1,9 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CliEntry {}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = CliEntry::parse();
 }
