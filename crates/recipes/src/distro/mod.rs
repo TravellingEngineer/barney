f210d6cf377c7d2588017131e13e24d9c43ad477
Ikey Doherty <travellingengineer@proton.me>
2026-09-12 21:35:38 +0100

    recipes/distro: Work with the crate types for errors etc
    Signed-off-by: Ikey Doherty <travellingengineer@proton.me>

--- crates/recipes/src/distro/mod.rs
+++ crates/recipes/src/distro/mod.rs
@@ -3,11 +3,27 @@
 
 //! Distro / OS definition cruft
 
+use std::{io, path::Path};
+
+use thiserror::Error;
+
 /// A Brogstrappa definition is taken from a brogstrappa.kdl
 /// You can blame Arjan van de Ven for this name
 ///
+#[derive(Debug)]
 pub struct Brogstrappa {}
 
-// impl Brogstrappa {
-//     pub fn from_path(whence: &Path) -> Result<Self, Error> {}
-// }
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+
+    #[error("unimplemented")]
+    Unimplemented,
+}
+
+impl Brogstrappa {
+    pub fn from_path(_whence: &impl AsRef<Path>) -> Result<Self, Error> {
+        Err(Error::Unimplemented)
+    }
+}
