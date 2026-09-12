62f987e425ebb96765858f49145ba03a24583cae
Ikey Doherty <travellingengineer@proton.me>
2026-09-12 21:51:52 +0100

    recipes/distro: Pipelining for KDL loading
    Signed-off-by: Ikey Doherty <travellingengineer@proton.me>

--- crates/recipes/src/distro/mod.rs
+++ crates/recipes/src/distro/mod.rs
@@ -3,8 +3,9 @@
 
 //! Distro / OS definition cruft
 
-use std::{io, path::Path};
+use std::{fs, io, path::Path, str::FromStr};
 
+use kdl::{KdlDocument, KdlError};
 use thiserror::Error;
 
 /// A Brogstrappa definition is taken from a brogstrappa.kdl
@@ -18,12 +19,30 @@ pub enum Error {
     #[error(transparent)]
     IoError(#[from] io::Error),
 
+    #[error(transparent)]
+    Parsing(#[from] KdlError),
+
     #[error("unimplemented")]
     Unimplemented,
 }
 
+impl FromStr for Brogstrappa {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let kdl_doc = KdlDocument::parse_v2(s)?;
+        Brogstrappa::new(&kdl_doc)
+    }
+}
+
 impl Brogstrappa {
-    pub fn from_path(_whence: &impl AsRef<Path>) -> Result<Self, Error> {
+    pub fn from_path(whence: &impl AsRef<Path>) -> Result<Self, Error> {
+        let contents = fs::read_to_string(whence)?;
+        let brog = contents.parse::<Brogstrappa>()?;
+        Ok(brog)
+    }
+
+    pub fn new(_doc: &KdlDocument) -> Result<Self, Error> {
         Err(Error::Unimplemented)
     }
 }
