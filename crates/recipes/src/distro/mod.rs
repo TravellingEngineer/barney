9bc530d0b1d8dd616a043a6f442f35296dc57ffe
Ikey Doherty <travellingengineer@proton.me>
2026-09-12 21:14:02 +0100

    recipes/distro: Add Brogstrappa scaffolding, pending error types
    Signed-off-by: Ikey Doherty <travellingengineer@proton.me>

--- crates/recipes/src/distro/mod.rs
+++ crates/recipes/src/distro/mod.rs
@@ -2,3 +2,12 @@
 // SPDX-License-Identifier: MPL-2.0
 
 //! Distro / OS definition cruft
+
+/// A Brogstrappa definition is taken from a brogstrappa.kdl
+/// You can blame Arjan van de Ven for this name
+///
+pub struct Brogstrappa {}
+
+// impl Brogstrappa {
+//     pub fn from_path(whence: &Path) -> Result<Self, Error> {}
+// }
