5ba919e9811013a07121668a13d1a08efb2aa9df
Ikey Doherty <travellingengineer@proton.me>
2026-09-15 20:02:59 +0100

    recipes: Support DSL errors via generic SyntaxError
    Signed-off-by: Ikey Doherty <travellingengineer@proton.me>

--- crates/recipes/src/distro/profile.rs
+++ crates/recipes/src/distro/profile.rs
@@ -12,32 +12,48 @@ use thiserror::Error;
 pub struct Profile {}
 
 #[derive(Debug, Error, Diagnostic)]
+pub enum SyntaxError {
+    #[error("unexpected property")]
+    UnexpectedProperty {
+        #[label("This should not be a property")]
+        span: SourceSpan,
+    },
+}
+
+#[derive(Debug, Error, Diagnostic)]
 pub enum Error {
-    #[error("Profiles can have one name only")]
-    ExactlyOne {
+    /// Error with the profiles name
+    #[error("Profiles must have a valid name")]
+    ProfileName {
         #[label("set a single name for this profile")]
         span: SourceSpan,
     },
 
+    /// TODO: Set fire to this crap
     #[error("not yet implemented")]
     #[diagnostic()]
     Unimplemented {
         #[label("not yet implemented!")]
         span: SourceSpan,
     },
+
+    #[error(transparent)]
+    #[diagnostic(transparent)]
+    Syntax(#[from] SyntaxError),
 }
 
 impl Profile {
+    /// Build a distro::Profile from a KdlNode
     pub(super) fn from_node(node: &KdlNode) -> Result<Self, Error> {
         let attr = node
             .entries()
             .iter()
             .exactly_one()
-            .map_err(|_| Error::ExactlyOne { span: node.span() })?;
+            .map_err(|_| Error::ProfileName { span: node.span() })?;
 
         // TODO: ParserError::UnexpectedProperty
         if attr.name().is_some() {
-            return Err(Error::Unimplemented { span: node.span() });
+            return Err(SyntaxError::UnexpectedProperty { span: node.span() })?;
         }
 
         // TODO: ParserError::UnexpectedType
