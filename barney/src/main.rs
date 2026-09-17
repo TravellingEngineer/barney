f050391f7b762dae527bb6415e6d347cef0272ff
Ikey Doherty <travellingengineer@proton.me>
2026-09-17 13:44:05 +0100

    barney: Integrate tracing/indicatif at primitive level
    Signed-off-by: Ikey Doherty <travellingengineer@proton.me>

--- barney/src/main.rs
+++ barney/src/main.rs
@@ -5,6 +5,9 @@ use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 use recipes::distro::Brogstrappa;
+use tracing::info;
+use tracing_indicatif::IndicatifLayer;
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -19,9 +22,33 @@ enum Commands {
     Build { distro: Option<PathBuf> },
 }
 
+// Initialisation of the tracing registry
+fn init_registry() {
+    let ind = IndicatifLayer::new();
+    let env = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    // TODO: Use some kind of timer
+    tracing_subscriber::registry()
+        .with(env)
+        .with(
+            fmt::layer()
+                .with_writer(ind.get_stderr_writer())
+                .pretty()
+                .compact()
+                .with_file(false)
+                .without_time()
+                .with_line_number(false),
+        )
+        .with(ind)
+        .init();
+}
+
 /// Main entry point
 #[tokio::main]
 async fn main() -> miette::Result<()> {
+    init_registry();
+    info!("Loading registry");
+
     let cli = CliEntry::parse();
 
     match &cli.command {
